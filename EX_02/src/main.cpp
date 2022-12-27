640bc6161b1f1a3baf7bebeb7efe4389ae770007
Ivan <ivan.chronos@gmail.com>
2022-12-27 19:08:20 -0300

    EX02 TaskDelete

---
+++ EX_02/src/main.cpp
@@ -0,0 +1,74 @@
+/*
+Exemplo para deletar uma task no FreeRTOS
+*/
+
+/*Biblioteca do Arduino*/
+#include <Arduino.h>
+
+/*Bibliotecas FreeRTOS*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*mapeamento de pinos*/
+#define LED 2
+
+/*Variáveis para armazenamento do handle das tasks*/
+TaskHandle_t task1Handle = NULL;
+TaskHandle_t task2Handle = NULL;
+
+/*protótipos das Tasks*/
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+
+void setup() {
+  Serial.begin(9600);
+  
+  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE,NULL,1,&task1Handle);
+  xTaskCreate(vTask2,"TASK2",configMINIMAL_STACK_SIZE+1024,NULL,2,&task2Handle);
+}
+
+void loop() {
+  vTaskDelay(1000);  
+}
+
+void vTask1(void *pvParameters)
+{
+  pinMode(LED,OUTPUT);
+
+  while (1)
+  {
+    digitalWrite(LED,!digitalRead(LED));
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
+}
+
+void vTask2(void *pvParameters)
+{
+  int cont = 0;
+  while (1)
+  {
+    Serial.println("Task 2: " + String(cont++));
+
+    if(cont >=10)
+    {
+      if(task1Handle != NULL)
+      {
+        Serial.println("Deletando Task 1");
+        vTaskDelete(task1Handle);
+        digitalWrite(LED,LOW);
+        task1Handle = NULL;
+      }
+    }
+
+    if(cont >=15)
+    {
+      if(task2Handle != NULL)
+      {
+        Serial.println("Deletando Task 2");
+        vTaskDelete(NULL);
+      }
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
